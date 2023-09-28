24a153f29269dfbbcfe5548bf33a64fdd3e98202
@bishal_sapkota <45955052+bishalsapkota0@users.noreply.github.com>
2023-09-28 01:01:32 -0500

    Add files via upload

---
+++ Java_Projects/Day-8/bubble.java
@@ -0,0 +1,28 @@
+/**
+ * bubble
+ */
+public class bubble {
+    public static void sort(int arr[]){ ///turn ==i 
+        for(int i= 0 ; i<arr.length-1;i++){
+            for(int j = 0 ; j<arr.length-1-i; j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]= temp;
+                }
+            }
+        }
+    } 
+    public static void print(int arr[]){
+          for(int i = 0 ; i<arr.length; i++)
+          {
+            System.out.print(arr[i]+" ");
+          }
+    }
+  public static void main(String[] args) {
+    int arr[]={2,8,5,3,9,4,1};
+    sort(arr);
+    print(arr);
+  }
+    
+}
